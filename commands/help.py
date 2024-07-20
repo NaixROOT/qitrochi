22c136c928732826288cb2c1b0dcc7323d5142ee
Vitaliy <118558563+Ghosvx@users.noreply.github.com>
2024-07-21 01:20:23 +0300

    ебу дал, sry

--- commands/help.py
+++ commands/help.py
@@ -9,14 +9,14 @@ def register_help_command(client):
 <b>Основные команды:</b>
 - <code>Xping</code>: Пинг Понг!
 - <code>Xtime [секунды]</code>: Вывод текущего времени или, если есть аргументы переводит секунды в ЧЧ:ММ:СС 
-- <code>Xuptime</code>: Время работы бота.
+- <code>Xuptime</code>: Время работы селфбота.
 - <code>Xinfo</code>: Информация о селфботе.
 
 <b>!!клепать аватарки тут!!:</b>
 - <code>Xava</code>: Зацикливает установку вашей аватарки.
 
 <b>Управление ботом:</b>
-- <code>Xrestart</code>: Перезапуск бота.
-- <code>Xstop</code>: Остановка работы бота.
+- <code>Xrestart</code>: Перезапуск селфбота.
+- <code>Xstop</code>: Остановка работы селфбота.
         """
         await event.edit(help_text, parse_mode='html')
